28f3b20aa363e6228a7c44cf3dc12db128a1a66f
The Grey Ghost <spamblackhole88@yahoo.com.au>
2015-11-16 23:31:12 +1030

    mbe06-redstone-started

---
+++ src/main/java/minecraftbyexample/mbe06_redstone/BlockRedstoneSource.java
@@ -0,0 +1,87 @@
+package minecraftbyexample.mbe06_redstone;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumWorldBlockLayer;
+import net.minecraft.world.IBlockAccess;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+/**
+ * User: The Grey Ghost
+ * Date: 24/12/2014
+ *
+ * BlockCompressedPowered()
+ *
+ */
+public class BlockRedstoneSource extends Block
+{
+  public BlockRedstoneSource()
+  {
+    super(Material.rock);
+    this.setCreativeTab(CreativeTabs.tabBlock);   // the block will appear on the Blocks tab in creative
+  }
+
+  @Override
+  public boolean canProvidePower()
+  {
+    return true;
+  }
+
+  /** The name of this method is rubbish
+   *
+   */
+  @Override
+  public int isProvidingWeakPower(IBlockAccess worldIn, BlockPos pos, IBlockState state, EnumFacing side)
+  {
+    return 15;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // the block will render in the SOLID layer.  See http://greyminecraftcoder.blogspot.co.at/2014/12/block-rendering-18.html for more information.
+  @SideOnly(Side.CLIENT)
+  public EnumWorldBlockLayer getBlockLayer()
+  {
+    return EnumWorldBlockLayer.SOLID;
+  }
+
+  // used by the renderer to control lighting and visibility of other blocks.
+  // set to true because this block is opaque and occupies the entire 1x1x1 space
+  // not strictly required because the default (super method) is true
+  @Override
+  public boolean isOpaqueCube() {
+    return true;
+  }
+
+  // used by the renderer to control lighting and visibility of other blocks, also by
+  // (eg) wall or fence to control whether the fence joins itself to this block
+  // set to true because this block occupies the entire 1x1x1 space
+  // not strictly required because the default (super method) is true
+  @Override
+  public boolean isFullCube() {
+    return true;
+  }
+
+  // render using a BakedModel (mbe01_block_simple.json --> mbe01_block_simple_model.json)
+  // not strictly required because the default (super method) is 3.
+  @Override
+  public int getRenderType() {
+    return 3;
+  }
+}
