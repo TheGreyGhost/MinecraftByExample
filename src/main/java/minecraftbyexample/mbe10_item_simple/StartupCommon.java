19b95d30b35bdd15f2260f27907cd493d168ccd8
TheGreyGhost <spamblackhole88@yahoo.com.au>
2016-08-09 23:01:48 +0930

    updating item transforms

--- src/main/java/minecraftbyexample/mbe10_item_simple/StartupCommon.java
+++ src/main/java/minecraftbyexample/mbe10_item_simple/StartupCommon.java
@@ -21,7 +21,18 @@ public class StartupCommon
 
   public static void preInitCommon()
   {
-    // each instance of your item should have a name that is unique within your mod.  use lower case.
+    // each instance of your item should have two names:
+    // 1) a registry name that is used to uniquely identify this item.  Should be unique within your mod.  use lower case.
+    // 2) an 'unlocalised name' that is used to retrieve the text name of your item in the player's language.  For example-
+    //    the unlocalised name might be "water", which is printed on the user's screen as "Wasser" in German or
+    //    "aqua" in Italian.
+    //
+    //    Multiple items can have the same unlocalised name - for example
+    //  +----RegistryName-------+----UnlocalisedName----+
+    //  |  burning_candle       +       candle          |
+    //  |  extinguished_candle  +       candle          |
+    //  +-----------------------+-----------------------+
+    //
     itemSimple = (ItemSimple)(new ItemSimple().setUnlocalizedName("mbe10_item_simple_unlocalised_name"));
     itemSimple.setRegistryName("mbe10_item_simple_registry_name");
     GameRegistry.register(itemSimple);
