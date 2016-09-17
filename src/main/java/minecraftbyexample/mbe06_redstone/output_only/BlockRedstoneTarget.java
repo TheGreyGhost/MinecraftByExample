7cf29f14ec146a0a534ac4b8a7ad835855a380f4
TheGreyGhost <spamblackhole88@yahoo.com.au>
2016-09-17 10:27:51 +0930

    mbe06 complete

--- src/main/java/minecraftbyexample/mbe06_redstone/output_only/BlockRedstoneTarget.java
+++ src/main/java/minecraftbyexample/mbe06_redstone/output_only/BlockRedstoneTarget.java
@@ -114,7 +114,8 @@ public class BlockRedstoneTarget extends Block
     if (!worldIn.isRemote) {
       if (entityIn instanceof EntityArrow) {
         AxisAlignedBB targetAABB = getCollisionBoundingBox(state, worldIn, pos);
-        List<EntityArrow> embeddedArrows = worldIn.getEntitiesWithinAABB(EntityArrow.class, targetAABB);
+        AxisAlignedBB targetAABBinWorld = targetAABB.offset(pos);
+        List<EntityArrow> embeddedArrows = worldIn.getEntitiesWithinAABB(EntityArrow.class, targetAABBinWorld);
 
         // when a new arrow hits, remove all others which are already embedded
 
@@ -161,18 +162,19 @@ public class BlockRedstoneTarget extends Block
     final int MISS_VALUE = -1;
     EnumFacing targetFacing = (EnumFacing)state.getValue(PROPERTYFACING);
     AxisAlignedBB targetAABB = getCollisionBoundingBox(state, worldIn, pos);
-    List<EntityArrow> embeddedArrows = worldIn.getEntitiesWithinAABB(EntityArrow.class, targetAABB);
+    AxisAlignedBB targetAABBinWorld = targetAABB.offset(pos);
+    List<EntityArrow> embeddedArrows = worldIn.getEntitiesWithinAABB(EntityArrow.class, targetAABBinWorld);
     if (embeddedArrows.isEmpty()) return MISS_VALUE;
 
     double closestDistance = Float.MAX_VALUE;
     for (EntityArrow entity : embeddedArrows) {
       if (!entity.isDead && entity instanceof EntityArrow) {
         EntityArrow entityArrow = (EntityArrow) entity;
-        Vec3d hitLocation = getArrowIntersectionWithTarget(entityArrow, targetAABB);
+        Vec3d hitLocation = getArrowIntersectionWithTarget(entityArrow, targetAABBinWorld);
         if (hitLocation != null) {
-          Vec3d targetCentre = new Vec3d((targetAABB.minX + targetAABB.maxX) / 2.0,
-                                              (targetAABB.minY + targetAABB.maxY) / 2.0,
-                                              (targetAABB.minZ + targetAABB.maxZ) / 2.0
+          Vec3d targetCentre = new Vec3d((targetAABBinWorld.minX + targetAABBinWorld.maxX) / 2.0,
+                                              (targetAABBinWorld.minY + targetAABBinWorld.maxY) / 2.0,
+                                              (targetAABBinWorld.minZ + targetAABBinWorld.maxZ) / 2.0
           );
           Vec3d hitRelativeToCentre = hitLocation.subtract(targetCentre);
 
@@ -310,14 +312,22 @@ public class BlockRedstoneTarget extends Block
   {
     return false;
   }
-  // render using a BakedModel (mbe01_block_simple.json --> mbe01_block_simple_model.json)
+
+  // render using a BakedModel
   // not strictly required because the default (super method) is MODEL.
   @Override
   public EnumBlockRenderType getRenderType(IBlockState iBlockState) {
     return EnumBlockRenderType.MODEL;
   }
 
-
+  /**
+   * Returns the borders of the target, depends on which way it is facing.
+   * Used by the vanilla getCollisionBox.
+   * @param state
+   * @param source
+   * @param pos
+   * @return the AxisAlignedBoundingBox of the target, origin at [0,0,0].
+   */
   @Override
   public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos)
   {
