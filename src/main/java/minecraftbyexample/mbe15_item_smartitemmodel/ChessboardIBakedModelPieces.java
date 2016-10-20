03cfafab3d5469e9ab91610ce00028bfe4a22493
The Grey Ghost <spamblackhole88@yahoo.com.au>
2016-10-20 23:19:21 +1030

    started on mbe15

---
+++ src/main/java/minecraftbyexample/mbe15_item_smartitemmodel/ChessboardIBakedModelPieces.java
@@ -0,0 +1,7 @@
+package minecraftbyexample.mbe15_item_smartitemmodel;
+
+/**
+ * Created by TGG on 20/10/2016.
+ */
+public class ChessboardIBakedModelPieces {
+}
---
+++ src/main/java/minecraftbyexample/mbe15_item_smartitemmodel/ChessboardItemOverrideList.java
@@ -0,0 +1,231 @@
+package minecraftbyexample.mbe15_item_smartitemmodel;
+
+import com.google.common.primitives.Ints;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.block.model.BakedQuad;
+import net.minecraft.client.renderer.block.model.IBakedModel;
+import net.minecraft.client.renderer.block.model.ItemOverride;
+import net.minecraft.client.renderer.block.model.ItemOverrideList;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by TGG on 20/10/2016.
+ */
+public class ChessboardItemOverrideList extends ItemOverrideList {
+  public ChessboardItemOverrideList(List<ItemOverride> overridesIn)
+  {
+    super(overridesIn);
+  }
+
+  public IBakedModel handleItemState(IBakedModel originalModel, ItemStack stack, World world, EntityLivingBase entity)
+  {
+
+  }
+
+  // return a list of BakedQuads for drawing the chess pieces
+  private List<BakedQuad> getChessPiecesQuads(int numberOfPieces)
+  {
+    final int MIN_NUMBER_OF_PIECES = 1;
+    final int PIECES_PER_ROW = 8;
+    final int NUMBER_OF_ROWS = 8;
+    final int MAX_NUMBER_OF_PIECES = PIECES_PER_ROW * NUMBER_OF_ROWS;
+
+    TextureAtlasSprite chessPieceTexture = Minecraft.getMinecraft().getTextureMapBlocks()
+            .getAtlasSprite("minecraft:blocks/diamond_block");
+    // if you want to use your own texture, you can add it to the texture map using code similar to this in your ClientProxy:
+    //   MinecraftForge.EVENT_BUS.register(new StitcherAddDigitsTexture());
+    //    public class StitcherAddDigitsTexture {
+    //      @SubscribeEvent
+    //      public void stitcherEventPre(TextureStitchEvent.Pre event) {
+    //        ResourceLocation digits = new ResourceLocation("stickmod:items/digits");
+    //        event.map.registerSprite(digits);
+    //      }
+    //    }
+
+    List<BakedQuad> returnList = new ArrayList<BakedQuad>(64);
+    if (numberOfPieces < MIN_NUMBER_OF_PIECES || numberOfPieces > MAX_NUMBER_OF_PIECES) {
+      return returnList;
+    }
+
+    // "builtin/generated" items, which are generated from the 2D texture by adding a thickness in the z direction
+    //    (i.e. north<-->south thickness) are centred around the z=0.5 plane.
+    final float BUILTIN_GEN_ITEM_THICKNESS = 1/16.0F;
+    final float BUILTIN_GEN_ITEM_Z_CENTRE = 0.5F;
+    final float BUILTIN_GEN_ITEM_Z_MAX = BUILTIN_GEN_ITEM_Z_CENTRE + BUILTIN_GEN_ITEM_THICKNESS / 2.0F;
+    final float BUILTIN_GEN_ITEM_Z_MIN = BUILTIN_GEN_ITEM_Z_CENTRE - BUILTIN_GEN_ITEM_THICKNESS / 2.0F;
+    final float SOUTH_FACE_POSITION = 1.0F;  // the south face of the cube is at z = 1.0F
+    final float NORTH_FACE_POSITION = 0.0F;  // the north face of the cube is at z = 0.0F
+    // http://greyminecraftcoder.blogspot.co.at/2014/12/blocks-18.html
+
+    final float DISTANCE_BEHIND_SOUTH_FACE = SOUTH_FACE_POSITION - BUILTIN_GEN_ITEM_Z_MAX;
+    final float DISTANCE_BEHIND_NORTH_FACE = BUILTIN_GEN_ITEM_Z_MIN - NORTH_FACE_POSITION;
+    final float FRACTION_OF_WIDTH_OCCUPIED_BY_A_PIECE = 0.75F;
+    final float FRACTION_OF_HEIGHT_OCCUPIED_BY_A_PIECE = 0.75F;
+    final float PIECE_WIDTH = 1.0F / PIECES_PER_ROW * FRACTION_OF_WIDTH_OCCUPIED_BY_A_PIECE;
+    final float PIECE_HEIGHT = 1.0F / NUMBER_OF_ROWS * FRACTION_OF_HEIGHT_OCCUPIED_BY_A_PIECE;
+
+    int row = 0;
+    int col = 0;
+    for (int i = 0; i < numberOfPieces; ++i) {
+      float columnCentrePosition = ((float)col + 0.5F)/PIECES_PER_ROW;
+      float rowCentrePosition = ((float)row + 0.5F)/NUMBER_OF_ROWS;
+
+      final int ITEM_RENDER_LAYER_NONE_SPECIFIED = -1;
+      final int ITEM_RENDER_LAYER0 = 0;
+      final int ITEM_RENDER_LAYER1 = 1;
+
+      // make a baked quad for each side of the chessboard i.e. front and back (south and north)
+      final float DELTA_FOR_OVERLAP = 0.001F;  // add a small overlap to stop the quad from lying exactly on top of the
+      //   existing face, which leads to "z-fighting" where the two quads
+      //   fight each other to be on top.  looks awful.
+
+      BakedQuad nextPieceFront = createBakedQuadForFace(columnCentrePosition, PIECE_WIDTH,
+              rowCentrePosition, PIECE_HEIGHT,
+              -DISTANCE_BEHIND_SOUTH_FACE + DELTA_FOR_OVERLAP,
+              ITEM_RENDER_LAYER0, chessPieceTexture, EnumFacing.SOUTH
+      );
+      BakedQuad nextPieceBack = createBakedQuadForFace(columnCentrePosition, PIECE_WIDTH,
+              rowCentrePosition, PIECE_HEIGHT,
+              -DISTANCE_BEHIND_NORTH_FACE + DELTA_FOR_OVERLAP,
+              ITEM_RENDER_LAYER0, chessPieceTexture, EnumFacing.NORTH
+      );
+      returnList.add(nextPieceFront);
+      returnList.add(nextPieceBack);
+
+      ++col;
+      if (col >= PIECES_PER_ROW) {
+        col = 0;
+        ++row;
+      }
+    }
+    return returnList;
+  }
+
+  /**
+   // Creates a baked quad for the given face.
+   // When you are directly looking at the face, the quad is centred at [centreLR, centreUD]
+   // The left<->right "width" of the face is width, the bottom<-->top "height" is height.
+   // The amount that the quad is displaced towards the viewer i.e. (perpendicular to the flat face you can see) is forwardDisplacement
+   //   - for example, for an EAST face, a value of 0.00 lies directly on the EAST face of the cube.  a value of 0.01 lies
+   //     slightly to the east of the EAST face (at x=1.01).  a value of -0.01 lies slightly to the west of the EAST face (at x=0.99).
+   // The orientation of the faces is as per the diagram on this page
+   //   http://greyminecraftcoder.blogspot.com.au/2014/12/block-models-texturing-quads-faces.html
+   // Read this page to learn more about how to draw a textured quad
+   //   http://greyminecraftcoder.blogspot.co.at/2014/12/the-tessellator-and-worldrenderer-18.html
+   * @param centreLR the centre point of the face left-right
+   * @param width    width of the face
+   * @param centreUD centre point of the face top-bottom
+   * @param height height of the face from top to bottom
+   * @param forwardDisplacement the displacement of the face (towards the front)
+   * @param itemRenderLayer which item layer the quad is on
+   * @param texture the texture to use for the quad
+   * @param face the face to draw this quad on
+   * @return
+   */
+  private BakedQuad createBakedQuadForFace(float centreLR, float width, float centreUD, float height, float forwardDisplacement,
+                                           int itemRenderLayer,
+                                           TextureAtlasSprite texture, EnumFacing face)
+  {
+    float x1, x2, x3, x4;
+    float y1, y2, y3, y4;
+    float z1, z2, z3, z4;
+    final float CUBE_MIN = 0.0F;
+    final float CUBE_MAX = 1.0F;
+
+    switch (face) {
+      case UP: {
+        x1 = x2 = centreLR + width/2.0F;
+        x3 = x4 = centreLR - width/2.0F;
+        z1 = z4 = centreUD + height/2.0F;
+        z2 = z3 = centreUD - height/2.0F;
+        y1 = y2 = y3 = y4 = CUBE_MAX + forwardDisplacement;
+        break;
+      }
+      case DOWN: {
+        x1 = x2 = centreLR + width/2.0F;
+        x3 = x4 = centreLR - width/2.0F;
+        z1 = z4 = centreUD - height/2.0F;
+        z2 = z3 = centreUD + height/2.0F;
+        y1 = y2 = y3 = y4 = CUBE_MIN - forwardDisplacement;
+        break;
+      }
+      case WEST: {
+        z1 = z2 = centreLR + width/2.0F;
+        z3 = z4 = centreLR - width/2.0F;
+        y1 = y4 = centreUD - height/2.0F;
+        y2 = y3 = centreUD + height/2.0F;
+        x1 = x2 = x3 = x4 = CUBE_MIN - forwardDisplacement;
+        break;
+      }
+      case EAST: {
+        z1 = z2 = centreLR - width/2.0F;
+        z3 = z4 = centreLR + width/2.0F;
+        y1 = y4 = centreUD - height/2.0F;
+        y2 = y3 = centreUD + height/2.0F;
+        x1 = x2 = x3 = x4 = CUBE_MAX + forwardDisplacement;
+        break;
+      }
+      case NORTH: {
+        x1 = x2 = centreLR - width/2.0F;
+        x3 = x4 = centreLR + width/2.0F;
+        y1 = y4 = centreUD - height/2.0F;
+        y2 = y3 = centreUD + height/2.0F;
+        z1 = z2 = z3 = z4 = CUBE_MIN - forwardDisplacement;
+        break;
+      }
+      case SOUTH: {
+        x1 = x2 = centreLR + width/2.0F;
+        x3 = x4 = centreLR - width/2.0F;
+        y1 = y4 = centreUD - height/2.0F;
+        y2 = y3 = centreUD + height/2.0F;
+        z1 = z2 = z3 = z4 = CUBE_MAX + forwardDisplacement;
+        break;
+      }
+      default: {
+        assert false : "Unexpected facing in createBakedQuadForFace:" + face;
+        return null;
+      }
+    }
+
+    return new BakedQuad(Ints.concat(vertexToInts(x1, y1, z1, Color.WHITE.getRGB(), texture, 16, 16),
+            vertexToInts(x2, y2, z2, Color.WHITE.getRGB(), texture, 16, 0),
+            vertexToInts(x3, y3, z3, Color.WHITE.getRGB(), texture, 0, 0),
+            vertexToInts(x4, y4, z4, Color.WHITE.getRGB(), texture, 0, 16)),
+            itemRenderLayer, face);
+  }
+
+  /**
+   * Converts the vertex information to the int array format expected by BakedQuads.
+   * @param x x coordinate
+   * @param y y coordinate
+   * @param z z coordinate
+   * @param color RGBA colour format - white for no effect, non-white to tint the face with the specified colour
+   * @param texture the texture to use for the face
+   * @param u u-coordinate of the texture (0 - 16) corresponding to [x,y,z]
+   * @param v v-coordinate of the texture (0 - 16) corresponding to [x,y,z]
+   * @return
+   */
+  private int[] vertexToInts(float x, float y, float z, int color, TextureAtlasSprite texture, float u, float v)
+  {
+    return new int[] {
+            Float.floatToRawIntBits(x),
+            Float.floatToRawIntBits(y),
+            Float.floatToRawIntBits(z),
+            color,
+            Float.floatToRawIntBits(texture.getInterpolatedU(u)),
+            Float.floatToRawIntBits(texture.getInterpolatedV(v)),
+            0
+    };
+  }
+
+}
