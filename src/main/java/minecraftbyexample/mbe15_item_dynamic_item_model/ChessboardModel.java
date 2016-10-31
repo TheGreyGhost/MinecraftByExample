bfb8ca0eb375f51ba5d47e42eb9ce34fd77a1acd
Matthew Cain <yooksi@zoho.com>
2016-10-31 21:52:51 +0100

    Format mbe15 code and comments
    Both code and comments should now be readable. I've also added HTML tags
    to class and method comments.

--- src/main/java/minecraftbyexample/mbe15_item_dynamic_item_model/ChessboardModel.java
+++ src/main/java/minecraftbyexample/mbe15_item_dynamic_item_model/ChessboardModel.java
@@ -13,96 +13,108 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * Created by TheGreyGhost on 19/04/2015.
- * This class modifies the displayed item (a chessboard) to show a number of "pieces" (blue squares) on the chessboard,
- *   one square for each item in the itemstack.
- * For models generated from a texture turned into a "texture with thickness" (i.e. like most items), you must implement
- *   IPerspectiveAwareModel instead of IBakedModel otherwise the item transforms won't work.  This is because Forge
- *   doesn't implement BakedItemModel.getItemCameraTransforms() correctly.
+ * User: TheGreyGhost <br> 
+ * Date: 19/04/2015   <p>
+ * 
+ * This class modifies the displayed item (a chessboard) to show a number of "pieces" (blue squares) 
+ * <br> on the chessboard, one square for each item in the itemstack. <p>
+ * 
+ * For models generated from a texture turned into a "texture with thickness" (i.e. like most items), 
+ * <br> you must implement IPerspectiveAwareModel instead of IBakedModel otherwise the item transforms <br>
+ *  won't work. This is because Forge doesn't implement BakedItemModel.getItemCameraTransforms() correctly.
  */
 public class ChessboardModel implements IPerspectiveAwareModel {
 
-  /**
-   * Create our model, using the given baked model as a base to add extra BakedQuads to.
-   * @param i_baseChessboardModel the base model
-   */
-  public ChessboardModel(IBakedModel i_baseChessboardModel)
-  {
-    baseChessboardModel = i_baseChessboardModel;
-    chessboardItemOverrideList = new ChessboardItemOverrideList(Collections.EMPTY_LIST);
-  }
+	/**
+	 * Create our model, using the given baked model as a base to add extra BakedQuads to.
+	 * @param i_baseChessboardModel the base model
+	 */
+	public ChessboardModel(IBakedModel i_baseChessboardModel)
+	{
+		baseChessboardModel = i_baseChessboardModel;
+		chessboardItemOverrideList = new ChessboardItemOverrideList(Collections.EMPTY_LIST);
+	}
 
-  // create a tag (ModelResourceLocation) for our model.
-  //  "inventory" is used for items. If you don't specify it, you will end up with "normal" by default,
-  //  which is used for blocks.
-  public static final ModelResourceLocation modelResourceLocation
-          = new ModelResourceLocation("minecraftbyexample:mbe15_item_chessboard", "inventory");
+	/* 
+	 *  Create a tag (ModelResourceLocation) for our model, "inventory" is used for items. 
+	 *  If you don't specify it, you will end up with "normal" by default, which is used for blocks.
+	 */
+	public static final ModelResourceLocation modelResourceLocation = 
+			new ModelResourceLocation("minecraftbyexample:mbe15_item_chessboard", "inventory");
 
-  @Override
-  public TextureAtlasSprite getParticleTexture() {
-    return baseChessboardModel.getParticleTexture();
-  }
+	@Override
+	public TextureAtlasSprite getParticleTexture() 
+	{
+		return baseChessboardModel.getParticleTexture();
+	}
 
-  /**  Returns the quads for the base chessboard model only
-   * @param state
-   * @param side
-   * @param rand
-   * @return
-   */
-  @Override
-  public List<BakedQuad> getQuads(IBlockState state, EnumFacing side, long rand) {
-    return baseChessboardModel.getQuads(state, side, rand);
-  }
+	/**  
+	 * Returns the quads for the base chessboard model only.
+	 */
+	@Override
+	public List<BakedQuad> getQuads(IBlockState state, EnumFacing side, long rand) 
+	{
+		return baseChessboardModel.getQuads(state, side, rand);
+	}
 
-  // not needed for items, but hey
-  @Override
-  public boolean isAmbientOcclusion() {
-    return baseChessboardModel.isAmbientOcclusion();
-  }
+	// not needed for items, but hey
+	@Override
+	public boolean isAmbientOcclusion() 
+	{
+		return baseChessboardModel.isAmbientOcclusion();
+	}
 
-  @Override
-  public boolean isGui3d() {
-    return baseChessboardModel.isGui3d();
-  }
+	@Override
+	public boolean isGui3d() 
+	{
+		return baseChessboardModel.isGui3d();
+	}
 
-  @Override
-  public boolean isBuiltInRenderer() {
-    return false;
-  }
+	@Override
+	public boolean isBuiltInRenderer() 
+	{
+		return false;
+	}
 
-  @Override
-  public ItemCameraTransforms getItemCameraTransforms() {
-    return baseChessboardModel.getItemCameraTransforms();  // NB this is not enough for BakedItemModels, must do handlePerspective as well
-  }
+	@Override
+	public ItemCameraTransforms getItemCameraTransforms() 
+	{
+		// NB this is not enough for BakedItemModels, must do handlePerspective as well.
+		return baseChessboardModel.getItemCameraTransforms();  
+	}
 
-  @Override
-  public ItemOverrideList getOverrides() {
-    return chessboardItemOverrideList;
-  }
+	@Override
+	public ItemOverrideList getOverrides() 
+	{
+		return chessboardItemOverrideList;
+	}
 
-  private IBakedModel baseChessboardModel;
-  private ChessboardItemOverrideList chessboardItemOverrideList;
+	private IBakedModel baseChessboardModel;
+	private ChessboardItemOverrideList chessboardItemOverrideList;
 
-  @Override
-  public Pair<? extends IBakedModel, Matrix4f> handlePerspective(ItemCameraTransforms.TransformType cameraTransformType) {
-    if (baseChessboardModel instanceof IPerspectiveAwareModel) {
-      Matrix4f matrix4f = ((IPerspectiveAwareModel)baseChessboardModel).handlePerspective(cameraTransformType).getRight();
-      return Pair.of(this, matrix4f);
-    } else {
-      // If the base model isn't an IPerspectiveAware, we'll need to generate the correct matrix ourselves using the
-      //  ItemCameraTransforms.
+	@Override
+	public Pair<? extends IBakedModel, Matrix4f> handlePerspective(ItemCameraTransforms.TransformType cameraTransformType) 
+	{
+		if (baseChessboardModel instanceof IPerspectiveAwareModel) 
+		{
+			Matrix4f matrix4f = ((IPerspectiveAwareModel)baseChessboardModel).handlePerspective(cameraTransformType).getRight();
+			return Pair.of(this, matrix4f);
+		} 
+		else 
+		{
+			// If the base model isn't an IPerspectiveAware, we'll need to generate the 
+			// correct matrix ourselves using the ItemCameraTransforms.
 
-      ItemCameraTransforms itemCameraTransforms = baseChessboardModel.getItemCameraTransforms();
-      ItemTransformVec3f itemTransformVec3f = itemCameraTransforms.getTransform(cameraTransformType);
-      TRSRTransformation tr = new TRSRTransformation(itemTransformVec3f);
-      Matrix4f mat = null;
-      if (tr != null) { // && tr != TRSRTransformation.identity()) {
-        mat = tr.getMatrix();
-      }
-      // The TRSRTransformation for vanilla items have blockCenterToCorner() applied, however handlePerspective
-      //  reverses it back again with blockCornerToCenter().  So we don't need to apply it here.
+			ItemCameraTransforms itemCameraTransforms = baseChessboardModel.getItemCameraTransforms();
+			ItemTransformVec3f itemTransformVec3f = itemCameraTransforms.getTransform(cameraTransformType);
+			TRSRTransformation tr = new TRSRTransformation(itemTransformVec3f);
+			
+			Matrix4f mat = tr != null ? tr.getMatrix() : null;
+			
+			// The TRSRTransformation for vanilla items have blockCenterToCorner() applied, however handlePerspective
+			// reverses it back again with blockCornerToCenter(), so we don't need to apply it here.
 
-      return Pair.of(this, mat);
-    }
-  }
+			return Pair.of(this, mat);
+		}
+	}
 }
--- src/main/java/minecraftbyexample/mbe15_item_dynamic_item_model/ItemChessBoard.java
+++ src/main/java/minecraftbyexample/mbe15_item_dynamic_item_model/ItemChessBoard.java
@@ -4,19 +4,19 @@ import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 
 /**
- * User: The Grey Ghost
- * Date: 30/12/2014
+ * User: The Grey Ghost <br>
+ * Date: 30/12/2014     <p>
  *
- * ItemChessBoard is an ordinary two-dimensional item
- * For background information on items see here http://greyminecraftcoder.blogspot.com/2013/12/items.html
- *   and here http://greyminecraftcoder.blogspot.com.au/2014/12/item-rendering-18.html
+ * ItemChessBoard is an ordinary two-dimensional item. <br>
+ * For background information on items see <a href="http://greyminecraftcoder.blogspot.com/2013/12/items.html">here</a>
+ * and <a href="http://greyminecraftcoder.blogspot.com.au/2014/12/item-rendering-18.html">here</a>.
  */
 public class ItemChessBoard extends Item
 {
-  public ItemChessBoard()
-  {
-    final int MAXIMUM_NUMBER_OF_COUNTERS = 64;
-    this.setMaxStackSize(MAXIMUM_NUMBER_OF_COUNTERS);
-    this.setCreativeTab(CreativeTabs.MISC);   // the item will appear on the Miscellaneous tab in creative
-  }
+	public ItemChessBoard()
+	{
+		final int MAXIMUM_NUMBER_OF_COUNTERS = 64;
+		this.setMaxStackSize(MAXIMUM_NUMBER_OF_COUNTERS);
+		this.setCreativeTab(CreativeTabs.MISC);
+	}
 }
