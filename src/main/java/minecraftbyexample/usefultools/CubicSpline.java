efa25c1892e8babcb9bac78f8667e967f367695f
TheGreyGhost <5291905+TheGreyGhost@users.noreply.github.com>
2020-07-12 00:22:17 +0930

    mbe81b splines

--- src/main/java/minecraftbyexample/usefultools/CubicSpline.java
+++ src/main/java/minecraftbyexample/usefultools/CubicSpline.java
@@ -26,9 +26,10 @@ import java.util.List;
  * https://en.wikipedia.org/wiki/Spline_interpolation
  *
  * Typical usage:
- * 1) Create an array of points that you want to interpolate [x,y]
- * 2) call createMonotoneCubicSpline to create the spline for those points [x,y]
- * 3) call interpolate to calculate the interpolated y for any given x
+ * 1) Create an array of points that you want to interpolate [t,x], where x is a function of t; for example
+ *    a graph of x position over time.  Must be in ascending order of t.
+ * 2) call createMonotoneCubicSpline to create the spline for those points [t,x]
+ * 3) call interpolate to calculate the interpolated x for any given t
  *
  */
 public class CubicSpline {
@@ -37,38 +38,38 @@ public class CubicSpline {
    * Creates a monotone cubic spline from a given set of control points.
    *
    * The spline is guaranteed to pass through each control point exactly. Moreover, assuming the control points are
-   * monotonic (Y is non-decreasing or non-increasing) then the interpolated values will also be monotonic.
+   * monotonic then the interpolated values will also be monotonic.
    *
    * This function uses the Fritsch-Carlson method for computing the spline parameters.
    * http://en.wikipedia.org/wiki/Monotone_cubic_interpolation
    *
+   * @param t
+   *            The t values of the control points, strictly increasing.
    * @param x
-   *            The X component of the control points, strictly increasing.
-   * @param y
-   *            The Y component of the control points
+   *            The t values corresponding to the given values of t
    * @return
    *
    * @throws IllegalArgumentException
-   *             if the X or Y arrays are null, have different lengths or have fewer than 2 values.
+   *             if the t or t arrays are null, have different lengths or have fewer than 2 values.
    */
-  public static CubicSpline createMonotoneCubicSpline(List<Float> x, List<Float> y) {
-    if (x == null || y == null || x.size() != y.size() || x.size() < 2) {
+  public static CubicSpline createMonotoneCubicSpline(List<Float> t, List<Float> x) {
+    if (t == null || x == null || t.size() != x.size() || t.size() < 2) {
       throw new IllegalArgumentException("There must be at least two control "
               + "points and the arrays must be of equal length.");
     }
 
-    final int n = x.size();
+    final int n = t.size();
     float[] d = new float[n - 1]; // could optimize this out
     float[] m = new float[n];
 
     // Compute slopes of secant lines between successive points.
     for (int i = 0; i < n - 1; i++) {
-      float h = x.get(i + 1) - x.get(i);
+      float h = t.get(i + 1) - t.get(i);
       if (h <= 0f) {
         throw new IllegalArgumentException("The control points must all "
-                + "have strictly increasing X values.");
+                + "have strictly increasing t values.");
       }
-      d[i] = (y.get(i + 1) - y.get(i)) / h;
+      d[i] = (x.get(i + 1) - x.get(i)) / h;
     }
 
     // Initialize the tangents as the average of the secants.
@@ -88,121 +89,113 @@ public class CubicSpline {
         float b = m[i + 1] / d[i];
         float h = (float) Math.hypot(a, b);
         if (h > 9f) {
-          float t = 3f / h;
-          m[i] = t * a * d[i];
-          m[i + 1] = t * b * d[i];
+          float u = 3f / h;
+          m[i] = u * a * d[i];
+          m[i + 1] = u * b * d[i];
         }
       }
     }
-    return new CubicSpline(x, y, m);
+    return new CubicSpline(t, x, m);
   }
 
   /**
-   * Interpolates the value of Y = f(X) for given X. Clamps X to the domain of the spline.
+   * Interpolates the value of X = f(t) for given t. Clamps t to the domain of the spline.
    *
-   * @param x
-   *            The X value.
-   * @return The interpolated Y = f(X) value.
+   * @param t The t value.
+   * @return The interpolated X = f(t) value.
    */
-  public float interpolate(float x) {
+  public float interpolate(float t) {
     // Handle the boundary cases.
-    final int n = mX.size();
-    if (Float.isNaN(x)) {
-      return x;
+    final int n = mT.size();
+    if (Float.isNaN(t)) {
+      return t;
     }
-    if (x <= mX.get(0)) {
-      return mY.get(0);
+    if (t <= mT.get(0)) {
+      return mX.get(0);
     }
-    if (x >= mX.get(n - 1)) {
-      return mY.get(n - 1);
+    if (t >= mT.get(n - 1)) {
+      return mX.get(n - 1);
+    }
+
+    // Find the index 'i' of the last point with smaller t.
+    // We know this will be within the spline due to the boundary tests.
+    int i = 0;
+    while (t >= mT.get(i + 1)) {
+      i += 1;
+      if (t == mT.get(i)) {
+        return mX.get(i);
+      }
+    }
+
+    // Perform cubic Hermite spline interpolation.
+    float h = mT.get(i + 1) - mT.get(i);
+    float u = (t - mT.get(i)) / h;
+    return (mX.get(i) * (1 + 2 * u) + h * mM[i] * u) * (1 - u) * (1 - u)
+            + (mX.get(i + 1) * (3 - 2 * u) + h * mM[i + 1] * (u - 1)) * u * u;
+  }
+
+  /**
+   * Interpolates the value of t = f'(t) for given t, i.e. the first derivative. Clamps t to the domain of the spline.
+   *
+   * @param t The t value.
+   * @return The interpolated t = f'(t) value.
+   */
+  public float interpolateFirstDerivative(float t) {
+    // Handle the boundary cases.
+    final int n = mT.size();
+    if (Float.isNaN(t)) {
+      return t;
+    }
+    if (t <= mT.get(0)) {
+      return mX.get(0);
+    }
+    if (t >= mT.get(n - 1)) {
+      return mX.get(n - 1);
     }
 
     // Find the index 'i' of the last point with smaller X.
     // We know this will be within the spline due to the boundary tests.
     int i = 0;
-    while (x >= mX.get(i + 1)) {
+    while (t >= mT.get(i + 1)) {
       i += 1;
-      if (x == mX.get(i)) {
-        return mY.get(i);
+      if (t == mT.get(i)) {
+        return mX.get(i);
       }
     }
 
     // Perform cubic Hermite spline interpolation.
-    float h = mX.get(i + 1) - mX.get(i);
-    float t = (x - mX.get(i)) / h;
-    return (mY.get(i) * (1 + 2 * t) + h * mM[i] * t) * (1 - t) * (1 - t)
-            + (mY.get(i + 1) * (3 - 2 * t) + h * mM[i + 1] * (t - 1)) * t * t;
+    float h = mT.get(i + 1) - mT.get(i);
+    float u = (t - mT.get(i)) / h;
+    return ( 3*mM[i + 1] - 6*mX.get(i+1) +3*mM[i] + 6*mX.get(i)) * u * u +
+           (-2*mM[i + 1] + 6*mX.get(i+1) -4*mM[i] - 6*mX.get(i)) * u +
+            mM[i];
   }
-
-  /**
-   * Interpolates the value of Y = f'(X) for given X, i.e. the first derivative. Clamps X to the domain of the spline.
-   *
-   * @param x The X value.
-   * @return The interpolated Y = f'(X) value.
-   */
-  public float interpolateFirstDerivative(float x) {
-    // Handle the boundary cases.
-    final int n = mX.size();
-    if (Float.isNaN(x)) {
-      return x;
-    }
-    if (x <= mX.get(0)) {
-      return mY.get(0);
-    }
-    if (x >= mX.get(n - 1)) {
-      return mY.get(n - 1);
-    }
-
-    // Find the index 'i' of the last point with smaller X.
-    // We know this will be within the spline due to the boundary tests.
-    int i = 0;
-    while (x >= mX.get(i + 1)) {
-      i += 1;
-      if (x == mX.get(i)) {
-        return mY.get(i);
-      }
-    }
-
-    // Perform cubic Hermite spline interpolation.
-    float h = mX.get(i + 1) - mX.get(i);
-    float t = (x - mX.get(i)) / h;
-    return (mY.get(i) * (1 + 2 * t) + h * mM[i] * t) * (1 - t) * (1 - t)
-            + (mY.get(i + 1) * (3 - 2 * t) + h * mM[i + 1] * (t - 1)) * t * t;
-  }
-
-  (3*d-6*c+3*b+6*a)*x^2+(-2*d+6*c-4*b-6*a)*x+b
-  where
-
-          (mYgeti * (1 + 2 * t) + h * mMi * t) * (1 - t) * (1 - t)
-          + (mYgetip1 * (3 - 2 * t) + h * mMip1 * (t - 1)) * t * t
-
-  a = mYgeti, b = mMi, c = mYgeip1, d = mMip1
 
   // For debugging.
   @Override
   public String toString() {
     StringBuilder str = new StringBuilder();
-    final int n = mX.size();
+    final int n = mT.size();
     str.append("[");
     for (int i = 0; i < n; i++) {
       if (i != 0) {
         str.append(", ");
       }
-      str.append("(").append(mX.get(i));
-      str.append(", ").append(mY.get(i));
+      str.append("(").append(mT.get(i));
+      str.append(", ").append(mX.get(i));
       str.append(": ").append(mM[i]).append(")");
     }
     str.append("]");
     return str.toString();
   }
 
+  private final List<Float> mT;
   private final List<Float> mX;
-  private final List<Float> mY;
   private final float[] mM;
 
-  private CubicSpline(List<Float> x, List<Float> y, float[] m) {
-    mX = x;
-    mY = y;
+  private CubicSpline(List<Float> t, List<Float> y, float[] m) {
+    mT = t;
+    mX = y;
     mM = m;
   }
 }
