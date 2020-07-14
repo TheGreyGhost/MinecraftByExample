8201244ea41c1c62e7f28fbe33b1ba9e468d2983
TheGreyGhost <5291905+TheGreyGhost@users.noreply.github.com>
2020-07-15 00:26:41 +0930

    mbe81b entity tick logic

--- src/main/java/minecraftbyexample/mbe81_entity_projectile/BoomerangEntity.java
+++ src/main/java/minecraftbyexample/mbe81_entity_projectile/BoomerangEntity.java
@@ -1,11 +1,18 @@
 package minecraftbyexample.mbe81_entity_projectile;
 
+import com.google.common.collect.Lists;
+import it.unimi.dsi.fastutil.ints.IntOpenHashSet;
 import minecraftbyexample.usefultools.NBTtypesMBE;
+import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.entity.projectile.AbstractArrowEntity;
 import net.minecraft.entity.projectile.ProjectileHelper;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.nbt.NBTUtil;
@@ -14,12 +21,15 @@ import net.minecraft.network.PacketBuffer;
 import net.minecraft.network.datasync.DataParameter;
 import net.minecraft.network.datasync.DataSerializers;
 import net.minecraft.network.datasync.EntityDataManager;
+import net.minecraft.network.play.server.SChangeGameStatePacket;
 import net.minecraft.particles.IParticleData;
 import net.minecraft.particles.ParticleTypes;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.*;
+import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.api.distmarker.Dist;
@@ -29,13 +39,14 @@ import net.minecraftforge.fml.network.NetworkHooks;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.Arrays;
 import java.util.Optional;
 import java.util.UUID;
 
 /**
  * Created by TGG on 24/06/2020.
  *
- * Heavily based on the vanilla SnowballEntity and ThrowableEntity
+ * Heavily based on the vanilla SnowballEntity, ThrowableEntity, and AbstractArrowEntity
  */
 public class BoomerangEntity extends Entity implements IEntityAdditionalSpawnData {
   public BoomerangEntity(EntityType<? extends BoomerangEntity> entityType, World world) {
@@ -353,6 +364,307 @@ public class BoomerangEntity extends Entity implements IEntityAdditionalSpawnDat
       this.setShotFromCrossbow(false);
       this.func_213870_w();
       blockstate.onProjectileCollision(this.world, blockstate, blockraytraceresult, this);
+    }
+
+  }
+  /**
+   * Called to update the entity's position/logic.
+   */
+  public void tick() {
+    super.tick();
+    boolean flag = this.getNoClip();
+    Vec3d vec3d = this.getMotion();
+    if (this.prevRotationPitch == 0.0F && this.prevRotationYaw == 0.0F) {
+      float f = MathHelper.sqrt(horizontalMag(vec3d));
+      this.rotationYaw = (float)(MathHelper.atan2(vec3d.x, vec3d.z) * (double)(180F / (float)Math.PI));
+      this.rotationPitch = (float)(MathHelper.atan2(vec3d.y, (double)f) * (double)(180F / (float)Math.PI));
+      this.prevRotationYaw = this.rotationYaw;
+      this.prevRotationPitch = this.rotationPitch;
+    }
+
+    BlockPos blockpos = new BlockPos(this);
+    BlockState blockstate = this.world.getBlockState(blockpos);
+    if (!blockstate.isAir(this.world, blockpos) && !flag) {
+      VoxelShape voxelshape = blockstate.getCollisionShape(this.world, blockpos);
+      if (!voxelshape.isEmpty()) {
+        Vec3d vec3d1 = this.getPositionVec();
+
+        for(AxisAlignedBB axisalignedbb : voxelshape.toBoundingBoxList()) {
+          if (axisalignedbb.offset(blockpos).contains(vec3d1)) {
+            this.inGround = true;
+            break;
+          }
+        }
+      }
+    }
+
+    if (this.arrowShake > 0) {
+      --this.arrowShake;
+    }
+
+    if (this.isWet()) {
+      this.extinguish();
+    }
+
+    if (this.inGround && !flag) {
+      if (this.inBlockState != blockstate && this.world.hasNoCollisions(this.getBoundingBox().grow(0.06D))) {
+        this.inGround = false;
+        this.setMotion(vec3d.mul((double)(this.rand.nextFloat() * 0.2F), (double)(this.rand.nextFloat() * 0.2F), (double)(this.rand.nextFloat() * 0.2F)));
+        this.ticksInGround = 0;
+        this.ticksInAir = 0;
+      } else if (!this.world.isRemote) {
+        this.func_225516_i_();
+      }
+
+      ++this.timeInGround;
+    } else {
+      this.timeInGround = 0;
+      ++this.ticksInAir;
+      Vec3d vec3d2 = this.getPositionVec();
+      Vec3d vec3d3 = vec3d2.add(vec3d);
+      RayTraceResult raytraceresult = this.world.rayTraceBlocks(new RayTraceContext(vec3d2, vec3d3, RayTraceContext.BlockMode.COLLIDER, RayTraceContext.FluidMode.NONE, this));
+      if (raytraceresult.getType() != RayTraceResult.Type.MISS) {
+        vec3d3 = raytraceresult.getHitVec();
+      }
+
+      while(!this.removed) {
+        EntityRayTraceResult entityraytraceresult = this.rayTraceEntities(vec3d2, vec3d3);
+        if (entityraytraceresult != null) {
+          raytraceresult = entityraytraceresult;
+        }
+
+        if (raytraceresult != null && raytraceresult.getType() == RayTraceResult.Type.ENTITY) {
+          Entity entity = ((EntityRayTraceResult)raytraceresult).getEntity();
+          Entity entity1 = this.getShooter();
+          if (entity instanceof PlayerEntity && entity1 instanceof PlayerEntity && !((PlayerEntity)entity1).canAttackPlayer((PlayerEntity)entity)) {
+            raytraceresult = null;
+            entityraytraceresult = null;
+          }
+        }
+
+        if (raytraceresult != null && raytraceresult.getType() != RayTraceResult.Type.MISS && !flag && !net.minecraftforge.event.ForgeEventFactory.onProjectileImpact(this, raytraceresult)) {
+          this.onHit(raytraceresult);
+          this.isAirBorne = true;
+        }
+
+        if (entityraytraceresult == null || this.getPierceLevel() <= 0) {
+          break;
+        }
+
+        raytraceresult = null;
+      }
+
+      vec3d = this.getMotion();
+      double d3 = vec3d.x;
+      double d4 = vec3d.y;
+      double d0 = vec3d.z;
+      if (this.getIsCritical()) {
+        for(int i = 0; i < 4; ++i) {
+          this.world.addParticle(ParticleTypes.CRIT, this.getPosX() + d3 * (double)i / 4.0D, this.getPosY() + d4 * (double)i / 4.0D, this.getPosZ() + d0 * (double)i / 4.0D, -d3, -d4 + 0.2D, -d0);
+        }
+      }
+
+      double d5 = this.getPosX() + d3;
+      double d1 = this.getPosY() + d4;
+      double d2 = this.getPosZ() + d0;
+      float f1 = MathHelper.sqrt(horizontalMag(vec3d));
+      if (flag) {
+        this.rotationYaw = (float)(MathHelper.atan2(-d3, -d0) * (double)(180F / (float)Math.PI));
+      } else {
+        this.rotationYaw = (float)(MathHelper.atan2(d3, d0) * (double)(180F / (float)Math.PI));
+      }
+
+      for(this.rotationPitch = (float)(MathHelper.atan2(d4, (double)f1) * (double)(180F / (float)Math.PI)); this.rotationPitch - this.prevRotationPitch < -180.0F; this.prevRotationPitch -= 360.0F) {
+        ;
+      }
+
+      while(this.rotationPitch - this.prevRotationPitch >= 180.0F) {
+        this.prevRotationPitch += 360.0F;
+      }
+
+      while(this.rotationYaw - this.prevRotationYaw < -180.0F) {
+        this.prevRotationYaw -= 360.0F;
+      }
+
+      while(this.rotationYaw - this.prevRotationYaw >= 180.0F) {
+        this.prevRotationYaw += 360.0F;
+      }
+
+      this.rotationPitch = MathHelper.lerp(0.2F, this.prevRotationPitch, this.rotationPitch);
+      this.rotationYaw = MathHelper.lerp(0.2F, this.prevRotationYaw, this.rotationYaw);
+      float f2 = 0.99F;
+      float f3 = 0.05F;
+      if (this.isInWater()) {
+        for(int j = 0; j < 4; ++j) {
+          float f4 = 0.25F;
+          this.world.addParticle(ParticleTypes.BUBBLE, d5 - d3 * 0.25D, d1 - d4 * 0.25D, d2 - d0 * 0.25D, d3, d4, d0);
+        }
+
+        f2 = this.getWaterDrag();
+      }
+
+      this.setMotion(vec3d.scale((double)f2));
+      if (!this.hasNoGravity() && !flag) {
+        Vec3d vec3d4 = this.getMotion();
+        this.setMotion(vec3d4.x, vec3d4.y - (double)0.05F, vec3d4.z);
+      }
+
+      this.setPosition(d5, d1, d2);
+      this.doBlockCollisions();
+    }
+  }
+
+  protected void func_225516_i_() {
+    ++this.ticksInGround;
+    if (this.ticksInGround >= 1200) {
+      this.remove();
+    }
+
+  }
+
+  /**
+   * Called when the arrow hits a block or an entity
+   */
+  protected void onHit(RayTraceResult raytraceResultIn) {
+    RayTraceResult.Type raytraceresult$type = raytraceResultIn.getType();
+    if (raytraceresult$type == RayTraceResult.Type.ENTITY) {
+      this.onEntityHit((EntityRayTraceResult)raytraceResultIn);
+    } else if (raytraceresult$type == RayTraceResult.Type.BLOCK) {
+      BlockRayTraceResult blockraytraceresult = (BlockRayTraceResult)raytraceResultIn;
+      BlockState blockstate = this.world.getBlockState(blockraytraceresult.getPos());
+      this.inBlockState = blockstate;
+      Vec3d vec3d = blockraytraceresult.getHitVec().subtract(this.getPosX(), this.getPosY(), this.getPosZ());
+      this.setMotion(vec3d);
+      Vec3d vec3d1 = vec3d.normalize().scale((double)0.05F);
+      this.setRawPosition(this.getPosX() - vec3d1.x, this.getPosY() - vec3d1.y, this.getPosZ() - vec3d1.z);
+      this.playSound(this.getHitGroundSound(), 1.0F, 1.2F / (this.rand.nextFloat() * 0.2F + 0.9F));
+      this.inGround = true;
+      this.arrowShake = 7;
+      this.setIsCritical(false);
+      this.setPierceLevel((byte)0);
+      this.setHitSound(SoundEvents.ENTITY_ARROW_HIT);
+      this.setShotFromCrossbow(false);
+      this.func_213870_w();
+      blockstate.onProjectileCollision(this.world, blockstate, blockraytraceresult, this);
+    }
+
+  }
+
+  private void func_213870_w() {
+    if (this.hitEntities != null) {
+      this.hitEntities.clear();
+    }
+
+    if (this.piercedEntities != null) {
+      this.piercedEntities.clear();
+    }
+
+  }
+
+  /**
+   * Called when the arrow hits an entity
+   */
+  protected void onEntityHit(EntityRayTraceResult p_213868_1_) {
+    Entity entity = p_213868_1_.getEntity();
+    float f = (float)this.getMotion().length();
+    int i = MathHelper.ceil(Math.max((double)f * this.damage, 0.0D));
+    if (this.getPierceLevel() > 0) {
+      if (this.piercedEntities == null) {
+        this.piercedEntities = new IntOpenHashSet(5);
+      }
+
+      if (this.hitEntities == null) {
+        this.hitEntities = Lists.newArrayListWithCapacity(5);
+      }
+
+      if (this.piercedEntities.size() >= this.getPierceLevel() + 1) {
+        this.remove();
+        return;
+      }
+
+      this.piercedEntities.add(entity.getEntityId());
+    }
+
+    if (this.getIsCritical()) {
+      i += this.rand.nextInt(i / 2 + 2);
+    }
+
+    Entity entity1 = this.getShooter();
+    DamageSource damagesource;
+    if (entity1 == null) {
+      damagesource = DamageSource.causeArrowDamage(this, this);
+    } else {
+      damagesource = DamageSource.causeArrowDamage(this, entity1);
+      if (entity1 instanceof LivingEntity) {
+        ((LivingEntity)entity1).setLastAttackedEntity(entity);
+      }
+    }
+
+    boolean flag = entity.getType() == EntityType.ENDERMAN;
+    int j = entity.getFireTimer();
+    if (this.isBurning() && !flag) {
+      entity.setFire(5);
+    }
+
+    if (entity.attackEntityFrom(damagesource, (float)i)) {
+      if (flag) {
+        return;
+      }
+
+      if (entity instanceof LivingEntity) {
+        LivingEntity livingentity = (LivingEntity)entity;
+        if (!this.world.isRemote && this.getPierceLevel() <= 0) {
+          livingentity.setArrowCountInEntity(livingentity.getArrowCountInEntity() + 1);
+        }
+
+        if (this.knockbackStrength > 0) {
+          Vec3d vec3d = this.getMotion().mul(1.0D, 0.0D, 1.0D).normalize().scale((double)this.knockbackStrength * 0.6D);
+          if (vec3d.lengthSquared() > 0.0D) {
+            livingentity.addVelocity(vec3d.x, 0.1D, vec3d.z);
+          }
+        }
+
+        if (!this.world.isRemote && entity1 instanceof LivingEntity) {
+          EnchantmentHelper.applyThornEnchantments(livingentity, entity1);
+          EnchantmentHelper.applyArthropodEnchantments((LivingEntity)entity1, livingentity);
+        }
+
+        this.arrowHit(livingentity);
+        if (entity1 != null && livingentity != entity1 && livingentity instanceof PlayerEntity && entity1 instanceof ServerPlayerEntity) {
+          ((ServerPlayerEntity)entity1).connection.sendPacket(new SChangeGameStatePacket(6, 0.0F));
+        }
+
+        if (!entity.isAlive() && this.hitEntities != null) {
+          this.hitEntities.add(livingentity);
+        }
+
+        if (!this.world.isRemote && entity1 instanceof ServerPlayerEntity) {
+          ServerPlayerEntity serverplayerentity = (ServerPlayerEntity)entity1;
+          if (this.hitEntities != null && this.getShotFromCrossbow()) {
+            CriteriaTriggers.KILLED_BY_CROSSBOW.trigger(serverplayerentity, this.hitEntities, this.hitEntities.size());
+          } else if (!entity.isAlive() && this.getShotFromCrossbow()) {
+            CriteriaTriggers.KILLED_BY_CROSSBOW.trigger(serverplayerentity, Arrays.asList(entity), 0);
+          }
+        }
+      }
+
+      this.playSound(this.hitSound, 1.0F, 1.2F / (this.rand.nextFloat() * 0.2F + 0.9F));
+      if (this.getPierceLevel() <= 0) {
+        this.remove();
+      }
+    } else {
+      entity.setFireTimer(j);
+      this.setMotion(this.getMotion().scale(-0.1D));
+      this.rotationYaw += 180.0F;
+      this.prevRotationYaw += 180.0F;
+      this.ticksInAir = 0;
+      if (!this.world.isRemote && this.getMotion().lengthSquared() < 1.0E-7D) {
+        if (this.pickupStatus == AbstractArrowEntity.PickupStatus.ALLOWED) {
+          this.entityDropItem(this.getArrowStack(), 0.1F);
+        }
+
+        this.remove();
+      }
     }
 
   }
