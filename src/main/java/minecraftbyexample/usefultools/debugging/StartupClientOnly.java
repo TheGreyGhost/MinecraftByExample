44120956c09948943f1a023ac6cc3c8c76731100
TheGreyGhost <5291905+TheGreyGhost@users.noreply.github.com>
2020-11-08 10:33:23 +1030

    re-enable VoxelShapeHighlighter

--- src/main/java/minecraftbyexample/usefultools/debugging/StartupClientOnly.java
+++ src/main/java/minecraftbyexample/usefultools/debugging/StartupClientOnly.java
@@ -8,13 +8,11 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
  * User: The Grey Ghost
  * Date: 24/12/2014
  *
- *  No client-only events are needed for this example
- *  See MinecraftByExample class for more information
  */
 public class StartupClientOnly
 {
     @SubscribeEvent
-    public void onClientSetupEvent(FMLClientSetupEvent event) {
+    public static void onClientSetupEvent(FMLClientSetupEvent event) {
       MinecraftForge.EVENT_BUS.register(DebugBlockVoxelShapeHighlighter.class);
     }
 }
